c22a467a27f0439f09bd1ec6c694c2eccea5e235
Jicell <jicellcollado@Jicells-MacBook-Air.local>
2022-10-03 20:42:02 -0400

    completed the networking code required to finish the API Endpoint

--- Crypto Coins/Model Controller/CryptoModelController.swift
+++ Crypto Coins/Model Controller/CryptoModelController.swift
@@ -36,11 +36,33 @@ class CryptoController {
         
         print(finalURL)
         
+        let dataTask = URLSession.shared.dataTask(with: finalURL) { coinData, _, error in
+            guard let error = error else {completion(false); return }
+            guard let data = coinData else {completion(false); return }
+            
+            do {
+                if let topLevelArrayOfCoinDictionaries = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.fragmentsAllowed) as? [[String:String]] {
+                    for coinDictionary in topLevelArrayOfCoinDictionaries {
+                        if let id = coinDictionary.id, let symbol = coinDictionary.symbol, let name = coinDictionary.name {
+                            let parcedCoin = Coin(id: id, symbol: symbol, name: name)
+                            coins.append(parcedCoin)
+                        }
+                    }
+                    
+              }
+                completion(true)
+            } catch {
+                print("Error in Do/Try/Catch \(error.localizedDescription)")
+                completion(false)
+            }
+            
+            
+            
+            
+        }.resume()
+        
+        //dataTask(with: URL, completionHandler: (Data?, URLResponse?, Error?) -> Void)
     }
-    
-    
-    
-    
     
     
     
